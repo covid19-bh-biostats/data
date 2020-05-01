e896d7ac345ce8f2570523227532afa2ee038788
ceci <ceciliav@student.ethz.ch>
2020-05-01 10:13:47 +0200

    update spanish dataset

--- Spain/translate.R
+++ Spain/translate.R
@@ -7,8 +7,8 @@
 library(dplyr)
 
 # Read datasets
-data1 <- read.csv("Spain/nacional_covid19.csv") # National number of cases, deaths, hopitalised and UCI patients by day
-data2 <- read.csv("Spain/nacional_covid19_rango_edad.csv") # By age group and sex
+data1 <- read.csv("nacional_covid19.csv") # National number of cases, deaths, hopitalised and UCI patients by day
+data2 <- read.csv("nacional_covid19_rango_edad.csv") # By age group and sex
 
 # Rename variables names
 colnames(data1) <- c("date", "cases", "recovered", "deaths", "icu_cases", "hospitalized")
@@ -18,9 +18,9 @@ colnames(data2) <- c("date", "age_range", "sex", "confirmed_cases", "hospitaliza
 data2 <- data2%>%
   dplyr::mutate(age_range = gsub(" y [+]", "+", age_range),
                 sex = ifelse(sex == "ambos", "both", 
-                             ifelse (sex == "mujeres", women, 
+                             ifelse (sex == "mujeres", "women", 
                                      ifelse (sex == "hombres", "men", sex))))
 
 # Save translated datasets
-write.csv(data1, file = "Spain/nacional_covid19.csv", row.names = FALSE)
-write.csv(data2, file = "Spain/nacional_covid19_rango_edad.csv", row.names = FALSE)
+write.csv(data1, file = "nacional_covid19.csv", row.names = FALSE)
+write.csv(data2, file = "nacional_covid19_rango_edad.csv", row.names = FALSE)
